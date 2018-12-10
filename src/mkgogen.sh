ad721dcdbac90c954f409fd1fff5f8eba62e43eb
Dave Raffensperger <daveraff@google.com>
2018-12-10 08:52:33 -0500

    Add trace export grpc-gateway config (#77)
    Add API configuration file and auto-generate command and for HTTP/JSON -> gRPC gateway for the OpenCensus trace exporter service.
    
    This will enable the opencensus-service to implement an HTTP endpoint for exporting traces.

--- src/mkgogen.sh
+++ src/mkgogen.sh
@@ -4,6 +4,9 @@
 # go get -d github.com/census-instrumentation/opencensus-proto
 # to check in the repo to the GOAPTH.
 #
+# This also requires the grpc-gateway plugin.
+# See: https://github.com/grpc-ecosystem/grpc-gateway#installation
+#
 # To generate:
 #
 # cd $(go env GOPATH)/census-instrumentation/opencensus-proto
@@ -18,4 +21,5 @@ protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/stats/v1/stats.proto \
     && protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace_config.proto \
     && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/common/v1/common.proto \
     && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto \
-    && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto
+    && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
+    && protoc --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./opencensus/proto/agent/trace/v1/trace_service_http.yaml:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto
