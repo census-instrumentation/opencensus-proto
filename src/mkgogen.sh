cfe95db9f268b874bdbdff0b1d018b11d6640abf
Nail Islamov <nilebox@users.noreply.github.com>
2020-07-21 15:29:46 +1000

    Generate Go types using  protocolbuffers/protobuf-go (#218)

--- src/mkgogen.sh
+++ src/mkgogen.sh
@@ -14,14 +14,17 @@
 
 OUTDIR="$(go env GOPATH)/src"
 
-protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/stats/v1/stats.proto \
-    && protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/metrics/v1/metrics.proto \
-    && protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/resource/v1/resource.proto \
-    && protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace.proto \
-    && protoc --go_out=plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace_config.proto \
-    && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/common/v1/common.proto \
-    && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto \
-    && protoc -I=. --go_out=plugins=grpc:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
+protoc --go_out=$OUTDIR opencensus/proto/stats/v1/stats.proto \
+    && protoc --go_out=$OUTDIR opencensus/proto/metrics/v1/metrics.proto \
+    && protoc --go_out=$OUTDIR opencensus/proto/resource/v1/resource.proto \
+    && protoc --go_out=$OUTDIR opencensus/proto/trace/v1/trace.proto \
+    && protoc --go_out=$OUTDIR opencensus/proto/trace/v1/trace_config.proto \
+    && protoc -I=. --go_out=$OUTDIR opencensus/proto/agent/common/v1/common.proto \
+    && protoc -I=. --go_out=$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto \
+    && protoc -I=. --go_out=$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
+    && protoc -I=. --go-grpc_out=$OUTDIR opencensus/proto/agent/common/v1/common.proto \
+    && protoc -I=. --go-grpc_out=$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto \
+    && protoc -I=. --go-grpc_out=$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
     && protoc --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./opencensus/proto/agent/trace/v1/trace_service_http.yaml:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
     && protoc --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./opencensus/proto/agent/metrics/v1/metrics_service_http.yaml:$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto
 
